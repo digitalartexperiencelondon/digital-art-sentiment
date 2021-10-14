5fd907c111ce6ba8761942f38675b22c84f9277f
Sonia Batllori <sbatllori@hotmail.com>
2021-10-14 22:12:56 +0200

    clean up final version

---
+++ shaders/assets/fshader.glsl
@@ -0,0 +1,225 @@
+#define PI 3.141592653589793238462643
+#define TWO_PI 2.0 * PI
+#define erase(scene, mask) scene *(1. - mask)
+#define add(scene, object, color) erase(scene, object) + object *color
+
+#define parse256RGB(r, g, b)                                                   \
+  vec3(float(r) / 255.0, float(g) / 255.0, float(b) / 255.0)
+#define COLOR_ANGER parse256RGB(255, 0, 0)        // RED
+#define COLOR_CONTEMPT parse256RGB(255, 132, 136) // PALE RED/PINK
+#define COLOR_DISGUST parse256RGB(255, 62, 255)   // MAGENTA
+#define COLOR_FEAR parse256RGB(0, 153, 0)         // DARK GREEN
+#define COLOR_HAPPINESS parse256RGB(255, 255, 2)  // YELLOW
+#define COLOR_NEUTRAL parse256RGB(255, 255, 255)  // GREYSCALE/WHITE
+#define COLOR_SADNESS parse256RGB(82, 80, 255)    // DARK BLUE
+#define COLOR_SURPRISE parse256RGB(0, 192, 255)   // LIGHT BLUE
+#define BGCOLOR parse256RGB(200, 200, 200)
+
+#define NUM_SENTIMENTS 8
+#define BUBBLE_FADE_AMOUNT 1.5 // min=0.0 -> max=NUM_SENTIMENTS
+
+precision mediump float;
+
+varying vec4 vPosition;
+varying vec2 vTexCoord;
+
+uniform vec2 u_resolution;
+uniform float u_time;
+uniform float u_audio[NUM_SENTIMENTS];
+
+uniform int u_top1;
+uniform int u_top2;
+uniform float u_mean;
+
+struct Sentiment {
+  float amount;
+  vec3 color;
+  float speed;
+};
+
+struct Bubble {
+  float radius;
+  vec2 position;
+  vec3 color;
+};
+
+struct SceneObject {
+  float sdf;
+  vec3 color;
+};
+
+Sentiment getSentiment(float data[NUM_SENTIMENTS], int idx);
+
+SceneObject getTop2SentimentsPlasma(vec2 p) {
+  const int iterations = 2;
+  float speed = 0.5;
+  float t = speed * u_time;
+  float waveFrequency = 1.0;
+  float waveHeight = 1.0;
+  float disphaseFactor = 0.0;
+  float edgeDistortion = 0.2;
+
+  if (u_top1 == u_top2) {
+    waveFrequency = 2.0;
+  }
+
+  for (int i = 0; i < iterations; i++) {
+    vec2 pDisplacement;
+    pDisplacement.x = cos(p.y * waveFrequency + t - disphaseFactor);
+    pDisplacement.y = sin(p.x * waveFrequency + t - disphaseFactor);
+    pDisplacement *= waveHeight;
+    pDisplacement += edgeDistortion * vec2(-pDisplacement.y, pDisplacement.x);
+    p += pDisplacement;
+
+    waveFrequency *= 1.5;
+    speed *= 1.5;
+    waveHeight *= 1.5;
+    disphaseFactor += 0.5 * t;
+  }
+
+  // Time varying pixel color
+  vec3 colorTop1 = getSentiment(u_audio, u_top1).color;
+  vec3 colorTop2;
+  if (u_top1 != u_top2) {
+    colorTop2 = getSentiment(u_audio, u_top2).color;
+  } else {
+    colorTop2 = mix(colorTop1, COLOR_NEUTRAL, 0.1);
+    colorTop1 = mix(colorTop1, vec3(0.0), 0.2);
+  }
+  vec3 color = mix(colorTop2, colorTop1, p.y + 0.5);
+
+  SceneObject obj;
+  obj.sdf = 1.0;
+  obj.color = color;
+  return obj;
+}
+
+vec4 BubbleSDF(Bubble bubble, vec2 p) {
+  float d = bubble.radius / length(p - bubble.position);
+  return vec4(bubble.color * d, d);
+}
+
+SceneObject getSentimentBubbles(float sentimentAmountThreshold, vec2 p) {
+
+  float totalAlpha = 0.0;
+  vec3 totalColor = vec3(0.0);
+
+  // For each sentiment, create a bubble.
+  for (int idx = 0; idx < NUM_SENTIMENTS; idx++) {
+
+    // Get the sentiment data.
+    Sentiment sentiment = getSentiment(u_audio, idx);
+
+    // Initialize the bubble parameters.
+    // - position: each bubble is initialized in its corresponding position, so
+    // that all the bubbles create a circle.
+    // - radius: only sentiments with an amount higher that the given threshold
+    // are considered.
+    // - color: the color associated to the sentiment.
+    Bubble bubble;
+    bubble.position = vec2(0.0);
+    float theta = TWO_PI * float(idx) / float(NUM_SENTIMENTS);
+    bubble.position.x = 0.5 * cos(theta * sentiment.speed * u_time);
+    bubble.position.y = 0.5 * sin(sentiment.amount * theta + u_time);
+    bubble.radius = sentiment.amount <= sentimentAmountThreshold
+                        ? 0.0 // Discard smaller bubbles.
+                        : 1.5 * sentiment.amount;
+    bubble.radius *= bubble.radius; // Empathize the color in the SDF.
+    bubble.color = sentiment.color;
+
+    // Get the SDF (signed distance function) to the bubble and accumulate its
+    // color values.
+    vec4 bubbleSD = BubbleSDF(bubble, p);
+    totalAlpha += bubbleSD.a;
+    totalColor += bubbleSD.rgb;
+  }
+
+  // Fade the merged bubbles.
+  float totalShape = smoothstep(float(NUM_SENTIMENTS) - BUBBLE_FADE_AMOUNT,
+                                float(NUM_SENTIMENTS), totalAlpha);
+
+  SceneObject obj;
+  obj.sdf = totalShape;
+  obj.color = totalColor / totalAlpha;
+  return obj;
+}
+
+void main() {
+
+  vec2 p = 2.0 * vPosition.xy - 1.0;
+  p.x *= u_resolution.x / u_resolution.y;
+
+  vec3 scene;
+  SceneObject background = getTop2SentimentsPlasma(p);
+  SceneObject foreground = getSentimentBubbles(u_mean, p);
+  scene = add(scene, background.sdf, background.color);
+  scene = add(scene, foreground.sdf, foreground.color);
+
+  gl_FragColor = vec4(scene, 1.0);
+}
+
+Sentiment getSentiment(float data[NUM_SENTIMENTS], int idx) {
+  if (idx == 0) {
+    Sentiment anger;
+    anger.amount = data[0];
+    anger.color = COLOR_ANGER;
+    anger.speed = 1.0;
+    return anger;
+  }
+
+  if (idx == 1) {
+    Sentiment contempt;
+    contempt.amount = data[1];
+    contempt.color = COLOR_CONTEMPT;
+    contempt.speed = 0.8;
+    return contempt;
+  }
+
+  if (idx == 2) {
+    Sentiment disgust;
+    disgust.amount = data[2];
+    disgust.color = COLOR_DISGUST;
+    disgust.speed = 0.4;
+    return disgust;
+  }
+
+  if (idx == 3) {
+    Sentiment fear;
+    fear.amount = data[3];
+    fear.color = COLOR_FEAR;
+    fear.speed = 0.0;
+    return fear;
+  }
+
+  if (idx == 4) {
+    Sentiment happiness;
+    happiness.amount = data[4];
+    happiness.color = COLOR_HAPPINESS;
+    happiness.speed = 0.5;
+    return happiness;
+  }
+
+  if (idx == 5) {
+    Sentiment neutral;
+    neutral.amount = data[5];
+    neutral.color = COLOR_NEUTRAL;
+    neutral.speed = 0.2;
+    return neutral;
+  }
+
+  if (idx == 6) {
+    Sentiment sadness;
+    sadness.amount = data[6];
+    sadness.color = COLOR_SADNESS;
+    sadness.speed = 0.1;
+    return sadness;
+  }
+
+  if (idx == 7) {
+    Sentiment surprise;
+    surprise.amount = data[7];
+    surprise.color = COLOR_SURPRISE;
+    surprise.speed = 0.9;
+    return surprise;
+  }
+}
--- shaders/sketch.js
+++ shaders/sketch.js
@@ -1,34 +1,105 @@
+// Global variables
 const fps = 60;
 const fetchFrequencySeconds = 1;
 
+let printSentimentResponse = false;
+let sentimentResponse, sentimentResponseValues;
+let sentimentsToShader;
+
 let loadedShader;
 
-let sentimentResponse;
-let shaderSentiments;
+let localTestData;
 
-// let dataJson;
+// ****************************************************************************
+// DATA FETCHING AND PARSING
+// ****************************************************************************
+class SentimentResponse {
+  anger;
+  contempt;
+  disgust;
+  fear;
+  happiness;
+  neutral;
+  sadness;
+  surprise;
+  type;
+
+  // Example of expected data (JSON):
+  // {
+  //     "anger": 0.1,
+  //     "contempt": 0.2,
+  //     "disgust": null,
+  //     "fear": 0.0,
+  //     "happiness": 0.943333,
+  //     "neutral": 0.3,
+  //     "sadness": 0.1,
+  //     "surprise": 0.1,
+  //     "type": "audio"
+  // }
+  constructor(data) {
+    this.anger = data.anger ?? 0.0;
+    this.contempt = data.contempt ?? 0.0;
+    this.disgust = data.disgust ?? 0.0;
+    this.fear = data.fear ?? 0.0;
+    this.happiness = data.happiness ?? 0.0;
+    this.neutral = data.neutral ?? 0.0;
+    this.sadness = data.sadness ?? 0.0;
+    this.surprise = data.surprise ?? 0.0;
+    this.type = data.type ?? "unknown";
+  }
+
+  toArray() {
+    return Object.entries(this)
+      .filter(([key, _value]) => key !== "type")
+      .map(([_key, value]) => value);
+  }
+
+  fromArray(dataArray) {
+    Object.entries(this).forEach(
+      ([key, _value], idx) => (this[key] = dataArray[idx])
+    );
+  }
+
+  print() {
+    console.log(`
+    RED\t\t\t anger:\t\t ${this.anger}
+    PALE PINK\t contempt:\t ${this.contempt}
+    MAGENTA\t\t disgust:\t ${this.disgust}
+    DARK GREEN\t fear:\t\t ${this.fear}
+    YELLOW\t\t happines:\t ${this.happiness}
+    WHITE\t\t neutral:\t ${this.neutral}
+    DARK BLUE\t sadness:\t ${this.sadness}
+    LIGHT BLUE\t surprise:\t ${this.surprise}
+    `);
+  }
+}
 
 function fetchSentimentData() {
   console.log("Fetching data...");
 
+  // TODO: get request to the backend api instead of reading local test data.
   // let url = "TBD";
   // httpGet(url, "jsonp", false, function (response) {
-  //   sentimentResponse =  new SentimentResponse(response);
+  //   sentimentResponse =  //...
   // });
 
-  // Local testing:
+  // TODO: remove next block (it's just reading test input)
+  // sentimentResponse = new SentimentResponse(localTestData);
+  // const response = sentimentResponse.toSentimentsArray();
   const response = Array.from(Array(8)).map((x) => random(0.0, 1.0));
-  shaderSentiments = sentimentResponse ?? response;
-  sentimentResponse = response;
+  sentimentResponse = new SentimentResponse({});
+  sentimentResponse.fromArray(response);
+
+  sentimentsToShader = sentimentResponseValues ?? response;
+  sentimentResponseValues = response;
 }
 
+// ****************************************************************************
+// VISUALISATION
+// ****************************************************************************
 function preload() {
-  loadedShader = loadShader(
-    "assets/vertex_shader.glsl",
-    "assets/fragment_shader.glsl"
-  );
-
-  // dataJson = loadJSON("assets/test_data.json");
+  loadedShader = loadShader("assets/vshader.glsl", "assets/fshader.glsl");
+  localTestData = loadJSON("assets/test_data.json");
 }
 
 function setup() {
@@ -43,6 +114,7 @@ function draw() {
   // Fetch data every first frame of the interpolation
   if (interpolationFrame == 1) {
     fetchSentimentData();
+    printSentimentResponse = true;
   }
 
   // Do nothing until the data is fetched
@@ -50,17 +122,39 @@ function draw() {
     return;
   }
 
+  if (printSentimentResponse) {
+    sentimentResponse.print();
+    printSentimentResponse = false;
+  }
+
   // Interpolate the sentiment values from the last value sent to the shaders,
   // to the last fetched value.
-  shaderSentiments = sentimentResponse.map((amount, idx) => {
-    const lastAmount = shaderSentiments[idx];
+  sentimentsToShader = sentimentResponseValues.map((amount, idx) => {
+    const lastAmount = sentimentsToShader[idx];
     return map(interpolationFrame, 1, fetchFrequencyFrames, lastAmount, amount);
   });
 
+  // Sort the sentiments by amount and get the ordered indices.
+  const mean =
+    sentimentsToShader.reduce((amount, acc) => amount + acc) /
+    sentimentsToShader.length;
+
+  const topSentimentsIndices = sentimentsToShader
+    .map((amount, idx) => [amount, idx])
+    .sort()
+    .reverse();
+
+  const [_top1Amount, top1Idx] = topSentimentsIndices[0];
+  let [top2Amount, top2Idx] = topSentimentsIndices[1];
+  top2Idx = top2Amount < mean ? top1Idx : top2Idx;
+
   shader(loadedShader);
   loadedShader.setUniform("u_resolution", [width, height]);
-  loadedShader.setUniform("u_time", frameCount);
-  loadedShader.setUniform("u_audio", shaderSentiments);
+  loadedShader.setUniform("u_time", 0.01 * frameCount);
+  loadedShader.setUniform("u_audio", sentimentsToShader);
+  loadedShader.setUniform("u_top1", top1Idx);
+  loadedShader.setUniform("u_top2", top2Idx);
+  loadedShader.setUniform("u_mean", mean);
   rect(0, 0, width, height);
 }
 
