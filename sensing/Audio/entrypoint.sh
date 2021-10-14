9330b75a94fb92960b82c672f0641c72ad6ab394
sonal-tandon <almond.7973@gmail.com>
2021-10-14 18:15:21 +0100

    Adding notes to entrypoint.sh script to describe what is left to do@

--- sensing/Audio/entrypoint.sh
+++ sensing/Audio/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+#Move into Dockerfile when git repo is stable
 git clone https://github.com/digitalartexperiencelondon/digital-art-sentiment.git
 
 cd digital-art-sentiment
@@ -10,5 +11,8 @@ chmod +x pyaudio.sh
 
 #This needs to be done in the entrypoint script provided you have mounted the volume
 cp -a /records/. /digital-art-sentiment/MevonAI-Speech-Emotion-Recognition/src/input
+
+#python3 speechEmotionRecognition.py
+# Something needs to run either here or in speechEmotionRecognition.py to turn the outputted sentiments into the right format and push it to the database
 
 /bin/bash
