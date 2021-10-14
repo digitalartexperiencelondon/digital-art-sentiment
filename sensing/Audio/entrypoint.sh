88ba64ad774edc52a9a6675bb14869b87ca0d75d
sonal-tandon <almond.7973@gmail.com>
2021-10-14 18:01:04 +0100

    Removing original sample files and changing entrypoint.sh to use new branch

--- sensing/Audio/entrypoint.sh
+++ sensing/Audio/entrypoint.sh
@@ -3,12 +3,12 @@
 git clone https://github.com/digitalartexperiencelondon/digital-art-sentiment.git
 
 cd digital-art-sentiment
-git checkout audio-input
+git checkout ML-Docker
 cd MevonAI-Speech-Emotion-Recognition/src
 chmod +x pyaudio.sh
 ./pyaudio.sh
 
-#This needs to be done in the entrypoint script provided you have moun
-cp -a /records/. /digital-art-sentiment/MevonAI-Speech-Emotion-Recognition/src/
+#This needs to be done in the entrypoint script provided you have mounted the volume
+cp -a /records/. /digital-art-sentiment/MevonAI-Speech-Emotion-Recognition/src/input
 
 /bin/bash
