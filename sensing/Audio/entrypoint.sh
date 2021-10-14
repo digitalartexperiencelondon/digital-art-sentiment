f82c3617b4153e5dd31f558723d7e8685c2adf54
sonal-tandon <almond.7973@gmail.com>
2021-10-14 13:29:50 +0100

    Moving git clone into entrypoint.sh

---
+++ sensing/Audio/entrypoint.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/digitalartexperiencelondon/digital-art-sentiment.git
+
+cd digital-art-sentiment
+git checkout Mevon-AI
+cd MevonAI-Speech-Emotion-Recognition/src
+chmod +x pyaudio.sh
+./pyaudio.sh
